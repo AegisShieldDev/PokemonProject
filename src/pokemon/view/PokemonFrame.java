bfa5308cfb5a9549181dddaf5fb025a74ffbf19d
Outrider99 <outrider.w@gmail.com>
2017-12-12 13:03:15 -0700

    Initial commit

---
+++ src/pokemon/view/PokemonFrame.java
@@ -0,0 +1,44 @@
+package pokemon.view;
+
+import pokemon.controller.PokemonController;
+import pokemon.view.PokemonPanel;
+import javax.swing.JFrame;
+
+public class PokemonFrame extends JFrame
+{
+	private PokemonController appController;
+	private PokemonPanel appPanel;
+	
+	/**
+	 * creates the PokemonFrame, calls setupframe
+	 * @param PokemonController appController
+	 */
+	public PokemonFrame(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		appPanel = new PokemonPanel(appController);
+		setupFrame();
+	}
+	
+	/**
+	 * Sets the settings for the PokemonFrame
+	 */
+	private void setupFrame()
+	{	
+		this.setContentPane(appPanel);
+		this.setSize(600,600);
+		this.setTitle("Gotta catch em all");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+	
+	/**
+	 * Getter for the appController
+	 * @return
+	 */
+	public PokemonController getBaseController()
+	{
+		return(appController);
+	}
+}
---
+++ src/pokemon/view/PokemonPanel.java
@@ -0,0 +1,12 @@
+package pokemon.view;
+
+import pokemon.controller.*;
+import javax.swing.JPanel;
+
+public class PokemonPanel extends JPanel
+{
+	public PokemonPanel(PokemonController appController)
+	{
+		
+	}
+}
