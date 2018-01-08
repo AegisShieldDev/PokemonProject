bcd35b35c34de7f8587dfc8984c5a51e91777f6a
Outrider99 <outrider.w@gmail.com>
2018-01-08 11:57:09 -0700

    Added PokemonPanel and pokedex List

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,9 +1,45 @@
 package pokemon.controller;
 
+
+import pokemon.model.Pokemon;
+import pokemon.view.PokemonFrame;
+import pokemon.model.*;
+import java.util.List;
+
+import java.util.ArrayList;
+
 public class PokemonController
 {
-	public void start()
+	private List<Pokemon> pokedex;
+	public PokemonController()
+	{
+		pokedex = new ArrayList<Pokemon>();
+		
+		PokemonFrame myFrame = new PokemonFrame(this);
+	}
+	public List<Pokemon> getPokedex()
+	{
+		return pokedex;
+	}
+	
+	public boolean isValidInteg(String input)
+	{
+		return false;
+	}
+	
+	public boolean isValidDouble(String input)
+	{
+		return false;
+	}
+	
+	public void buildPokedex()
 	{
 		
+		pokedex.add(new Xatu(1,""));
+	}
+	
+	public void start()
+	{
+		buildPokedex();
 	}
 }
--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,12 +1,50 @@
 package pokemon.view;
 
 import pokemon.controller.*;
-import javax.swing.JPanel;
+import javax.swing.*;
+
+import pokemon.controller.PokemonController;
 
 public class PokemonPanel extends JPanel
 {
-	public PokemonPanel(PokemonController appController)
+	private PokemonController appController;
+	private SpringLayout appLayout;
+	
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	private JLabel nameLabel;
+	private JLabel evolvableLable;
+	private JLabel modiferLabel;
+	private JLabel iconLabel;
+	
+	private JCheckBox evolvableBox;
+	
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextField attackField;
+	private JTextField healthField;
+	private JTextField modifierField;
+	
+	private JTextArea descriptionArea;
+	private JTextArea typeArea;
+	
+	private JButton saveButton;
+	private JButton clearButton;
+	
+	private JComboBox pokedexDropdown;
+	
+	private JPanel firstType;
+	private JPanel secondType;
+	private JPanel thirdType;
+	private JPanel fourthType;
+
+	private void updatePokedexInfo(int index)
 	{
-		
+		nameField.setText(appController.getPokedex().get(index).getName());
+		evolvableBox.setSelected(appController.getPokedex().get(index).isCanEvolve());
+		numberField.setText(appController.getPokedex().get(index).getNumber() + "");
+		attackField.setText(appController.getPokedex().get(index).getAttackPoints() + "");
+		healthField.setText(appController.getPokedex().get(index).getHealthPoints() + "");
+		modifierField.setText(appController.getPokedex().get(index).getEnhancementModifier() + "");
 	}
 }
